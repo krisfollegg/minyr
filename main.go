45d35b239644dbbcc11b4eff30686a460611ff15
krisfollegg <kris.follegg@gmail.com>
2023-04-05 21:36:55 +0200

    endringer

--- main.go
+++ main.go
@@ -1,12 +1,117 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+	"log"
+    "os"
+    "strconv"
+    "strings"
     "github.com/krisfollegg/funtemps/conv"
 )
-
+const (
+    inputFileName  = "kjevik-temp-celsius-20220318-20230318.csv"
+    outputFileName = "kjevik-tempfahr-20220318-20230318.csv"
+)
 func main() {
+    reader := bufio.NewReader(os.Stdin)
+    fmt.Println("Welcome to the Minyr program!")
+    for {
+        fmt.Println("\nPlease select an option:")
+        fmt.Println("1. Convert Celsius to Fahrenheit")
+        fmt.Println("2. Calculate average temperature")
+        fmt.Println("3. Quit")
+        option, _ := reader.ReadString('\n')
+        option = strings.TrimSpace(option)
+        switch option {
+        case "1":
+            convertCelsiusToFahrenheit()
+        case "2":
+            calculateAverageTemperature()
+        case "3":
+            fmt.Println("Goodbye!")
+            return
+        default:
+            fmt.Println("Invalid option, please try again.")
+        }
+    }
 }
-
-
-
+func convertCelsiusToFahrenheit() {
+    fmt.Println("Converting Celsius to Fahrenheit...")
+    inputFile, err := os.Open(inputFileName)
+    if err != nil {
+		log.Fatal(err)
+        return
+    }
+    defer inputFile.Close()
+    outputFile, err := os.Create(outputFileName)
+    if err != nil {
+		log.Fatal(err)
+        return
+    }
+    defer outputFile.Close()
+    scanner := bufio.NewScanner(inputFile)
+    writer := bufio.NewWriter(outputFile)
+    // First line is header, copy as is
+    if scanner.Scan() {
+        writer.WriteString(scanner.Text() + "\n")
+    }
+    for scanner.Scan() {
+        line := scanner.Text()
+        fields := strings.Split(line, ";")
+        if len(fields) != 4 {
+            fmt.Println("Invalid line:", line)
+            continue
+        }
+        tempCelsius, err := strconv.ParseFloat(fields[3], 64)
+        if err != nil {
+			log.Fatal(err)
+            continue
+        }
+        tempFahrenheit := conv.CelsiusToFahrenheit(tempCelsius)
+        fields[3] = strconv.FormatFloat(tempFahrenheit, 'f', 1, 64)
+        writer.WriteString(strings.Join(fields, ";") + "\n")
+    }
+    writer.Flush()
+    fmt.Println("Conversion complete. Results saved in", outputFileName)
+}
+func calculateAverageTemperature() {
+    fmt.Println("Calculating average temperature...")
+    inputFile, err := os.Open(inputFileName)
+    if err != nil {
+		log.Fatal(err)
+        return
+    }
+    defer inputFile.Close()
+    scanner := bufio.NewScanner(inputFile)
+    // First line is header, ignore
+    scanner.Scan()
+    var sumC float64
+    var sumF float64
+    count := 0
+    for scanner.Scan() {
+        line := scanner.Text()
+        fields := strings.Split(line, ";")
+        if len(fields) != 4 {
+            fmt.Println("Invalid line:", line)
+            continue
+        }
+        tempCelsius, err := strconv.ParseFloat(fields[3], 64)
+        if err != nil {
+			log.Fatal(err)
+            continue
+        }
+        sumC += tempCelsius
+        sumF += conv.CelsiusToFahrenheit(tempCelsius)
+        count++
+    }
+    if count == 0 {
+        fmt.Println("No valid temperature data found.")
+        return
+    }
+    avgCelsius := sumC / float64(count)
+    avgFahrenheit := sumF / float64(count)
+    fmt.Printf("Average temperature over %d days:\n", count)
+    fmt.Printf("  Celsius:    %.1f\n", avgCelsius)
+    fmt.Printf("  Fahrenheit: %.1f\n", avgFahrenheit)
+}
---
+++ yr/yr_test.go
@@ -0,0 +1,35 @@
+package yr
+
+import (
+	"bytes"
+	"encoding/csv"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertTemperatures(t *testing.T) {
+	err := ConvertTemperatures()
+	assert.NoError(t, err)
+
+	// Check that the output file has the correct number of lines
+	data, err := ioutil.ReadFile("kjevik-tempfahr-20220318-20230318.csv")
+	assert.NoError(t, err)
+	reader := csv.NewReader(bytes.NewReader(data))
+	lines, err := reader.ReadAll()
+	assert.NoError(t, err)
+	assert.Equal(t, 16756, len(lines))
+
+	// Check that the temperatures are converted correctly
+	expected := "Kjevik;SN39040;18.03.2022 01:50;42.8"
+	actual := lines[1][3]
+	assert.Equal(t, expected, actual)
+
+	expected = "Kjevik;SN39040;07.03.2023 18:20;32.0"
+	actual = lines[8368][3]
+	assert.Equal(t, expected, actual)
+
+	expected = "Kjevik;SN39040;08.03.202
