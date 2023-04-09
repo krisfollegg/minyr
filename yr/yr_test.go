6e8f4c4f76bcd4c268172e9a81a9e4ee721301fe
Kristian <112094788+krisfollegg@users.noreply.github.com>
2023-04-09 20:15:05 +0200

    Update yr_test.go

--- yr/yr_test.go
+++ yr/yr_test.go
@@ -14,14 +14,14 @@ func TestConvertTemperatures(t *testing.T) {
     err := ConvertTemperatures()
     assert.NoError(t, err)
 }
-    // Check that the output file has the correct number of lines
+    // Sjekker at output filen har riktig antall linjer
     data, err := ioutil.ReadFile("kjevik-temp-fahr-20220318-20230318.csv")
     assert.NoError(t, err)
     reader := csv.NewReader(bytes.NewReader(data))
     lines, err := reader.ReadAll()
     assert.NoError(t, err)
     assert.Equal(t, 16756, len(lines))
-    // Check that the temperatures are converted correctly
+    // Sjekker at temperaturen er konvertert riktig 
     expected := "Kjevik;SN39040;18.03.2022 01:50;42.8"
     actual := lines[1][3]
     assert.Equal(t, expected, actual)
